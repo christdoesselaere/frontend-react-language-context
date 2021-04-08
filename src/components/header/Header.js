5a690e2be038e32b536c960221f220b31b1ed57c
Christ Doesselaere <67807770+christdoesselaere@users.noreply.github.com>
2021-04-08 22:55:52 +0200

    Roadmap step 12 add 1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import headerImage from '../../assets/wall-with-plants.jpg';
 import { ReactComponent as FlagNL } from '../../assets/netherlands.svg';
 import content from '../../data/content.json';
 import './Header.css';
+import {LanguageContext} from "../../context/LanguageContext";
 
 function Header() {
+  const {activeLanguage} = useContext(LanguageContext);
+
   return (
     <>
       <header>
@@ -13,23 +16,23 @@ function Header() {
           <ul>
             <li>
               <NavLink exact to="/about-us">
-                {content.nl.header.menuItems.aboutUs}
+                {content[activeLanguage].header.menuItems.aboutUs}
               </NavLink>
             </li>
             <li>
               <NavLink exact to="/all-plants">
-                {content.nl.header.menuItems.allPlants}
+                {content[activeLanguage].header.menuItems.allPlants}
               </NavLink>
               </li>
             <li className="language-switch">
-              <p>{content.nl.header.changeTo}</p>
+              <p>{content[activeLanguage].header.changeTo}</p>
               <FlagNL />
             </li>
           </ul>
         </nav>
         <div className="image-container">
-          <img src={headerImage} alt="Header image plants" className="header-image" />
-          <h1>{content.nl.header.title}</h1>
+          <img src={headerImage} alt="Header plants" className="header-image" />
+          <h1>{content[activeLanguage].header.title}</h1>
         </div>
 
       </header>
